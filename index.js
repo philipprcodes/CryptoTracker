81226247b9fb10b6489c46ef2815ff3954304a29
Philipp <P.Rodowski@live.de>
2026-06-30 15:48:46 +0200

    feat: added Top Gainer and Top Loser

--- index.js
+++ index.js
@@ -2,19 +2,28 @@ import {fetchMarkets, fetchPrices} from "./lib/api.js";
 import {sortByChange} from "./lib/transform.js";
 import {printCoin} from "./lib/display.js";
 
-const COINS = ["bitcoin", "ethereum", "solana"];
+const COINS = ["bitcoin", "ethereum", "solana","ripple","dogecoin"];
 const CURRENCIES = ["eur", "usd"];
 
 
 async function main(){
     try {
         const markets = await fetchMarkets(COINS,"eur");
-        console.log("Unsortiert:\n");
+        console.log("==============================")
+        console.log("Unsortiert:");
         markets.forEach(printCoin);
-
+        console.log("==============================")
         const sortedByChange = sortByChange(markets, "desc");
-        console.log("\nSortiert nach 24h Aenderung:\n");
+        console.log("Sortiert nach 24h Aenderung:");
         sortedByChange.forEach(printCoin);
+
+        const topGainer = sortedByChange[0];
+        const topLoser = sortedByChange[sortedByChange.length - 1];
+        console.log("==============================")
+        console.log("Top Gainer:");
+        printCoin(topGainer);
+        console.log("Top Loser:");
+        printCoin(topLoser);
 
     }catch(error){
         console.error("Fehler beim Abrufen der Preise:", error.message);
